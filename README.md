e7b690207a5fe3ce20aa11ba4f1e92c7a7c5995f
baset-sarker <chayan2005@gmail.com>
2024-03-28 10:15:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# basetsarker.github.io
---
+++ js/data.js
@@ -0,0 +1,114 @@
+var data = {
+    "journals": [
+        {
+            'title': 'First Entry',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+            'type': 'journal'
+        },
+        {
+            'title': 'First Entry',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+            'type': 'journal'
+        }
+
+    ],
+    "conferences": [
+        {
+            'title': 'First Entry',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+            'type': 'conference'
+        },
+        {
+            'title': 'First Entry',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+            'type': 'conference'
+        }
+    ],
+
+    "projects": [
+        {
+            'title': 'First Entry',
+            'authors': 'John Doe',
+            'date': '2016-12-31',
+            'conference': 'Conference of Stuff',
+            'doi': '10.1234/5678',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+        },
+        {
+            'title': 'First Entry',
+            'authors': 'John Doe',
+            'date': '2016-12-31',
+            'conference': 'Conference of Stuff',
+            'doi': '10.1234/5678',
+            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
+            'abstract': 'This is the first entry in the conference. It is a test entry.'
+        }
+    ],
+
+    "news": [
+        {
+            'year': '2024',
+            'event':[
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                },
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                }
+            
+            ]
+        },
+        {
+            'year': '2023',
+            'event':[
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                },
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                }
+            
+            ]
+        },
+        {
+            'year': '2022',
+            'event':[
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                },
+                {
+                    'title': 'First Entry',
+                    'month': 'March 23',
+                }
+            
+            ]
+        },
+        {
+            'year': '2021',
+            'event':[
+                {
+                    'title': 'First Entry',
+                    'date': '01-01-2019',
+                },
+                {
+                    'title': 'First Entry',
+                    'date': '01-01-2019',
+                }
+            
+            ]
+        }
+        
+    ],
+    
+
+}
