0dabfc8f3eb5074b5f76ba93694470583e925d39
baset-sarker <chayan2005@gmail.com>
2024-03-28 22:55:43 -0400

    added data and modified pages to show the data

--- js/data.js
+++ js/data.js
@@ -1,17 +1,31 @@
 var data = {
+
     "journals": [
         {
-            'title': 'First Entry',
+            'authors': 'M. A. Baset Sarker, Juan Pablo Sola, Aaron Jones, Evan Laing, Ernesto S Sola, and Masudul H. Imtiaz,',
+            'title': 'Vision Controlled Sensorized Prosthetic Hand,',
+            'journal_or_conf': 'Interdisciplinary Conference on Mechanics, Computers and Electronics (ICMECE) 2022',
+            'rest_part': 'Albany, NY, USA, 2022, pp. 1-4, doi: 10.1109/MDTS54894.2022.9826969.',
+            'doi': '10.1234/5678',
             'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
-            'other_ref': 'This is the first entry in the conference. It is a test entry.',
+            'paper_link': '',
+            'source_code': '',
             'type': 'journal'
         },
+
+
         {
-            'title': 'First Entry',
-            'googl_scholar': 'https://scholar.google.com/scholar?q=10.1234/5678',
-            'other_ref': 'This is the first entry in the conference. It is a test entry.',
-            'type': 'journal'
-        }
+            'authors': 'E. Sola-Thomas, M. A. Baset Sarker and M. Imtiaz,',
+            'title': 'FPGA-Controlled AI Vision for Prosthetics hand,',
+            'journal_or_conf': 'Interdisciplinary Conference on Mechanics, Computers and Electronics (ICMECE) 2022',
+            'rest_part': '2023 IEEE World AI IoT Congress (AIIoT), Seattle, WA, USA, 2023, pp. 0520-0524, doi: 10.1109/AIIoT58121.2023.10174491.',
+            'doi': '10.1234/5678',
+            'googl_scholar': 'https://scholar.google.com/citations?view_op=view_citation&hl=en&user=1PhvGNEAAAAJ&citation_for_view=1PhvGNEAAAAJ:_FxGoFyzp5QC',
+            'paper_link': 'https://ieeexplore.ieee.org/abstract/document/10174491',
+            'source_code': '',
+            'type': 'conference'
+        },
+      
 
     ],
     "conferences": [
@@ -51,64 +65,114 @@ var data = {
     ],
 
     "news": [
-        {
-            'year': '2024',
-            'event':[
-                {
-                    'title': 'First Entry',
-                    'month': 'March 23',
-                },
-                {
-                    'title': 'First Entry',
-                    'month': 'March 23',
-                }
+        // {
+        //     'year': '2024',
+        //     'events':[
+        //         {
+        //             'month': 'March 23',
+        //             'title': 'year3 2323',
+                    
+        //         },
+        //         {
+        //             'month': 'March 23',
+        //             'title': 'First 2323',  
+        //         }
             
-            ]
-        },
+        //     ]
+        // },
+        // {
+        //     'year': '2023',
+        //     'events':[
+        //         {
+        //             'title': 'First Entry',
+        //             'month': 'March 23',
+        //         },
+        //         {
+        //             'title': 'First Entry',
+        //             'month': 'March 23',
+        //         }
+            
+        //     ]
+        // },
+        // {
+        //     'year': '2022',
+        //     'events':[
+        //         {
+        //             'title': 'First Entry',
+        //             'month': 'March 23',
+        //         },
+        //         {
+        //             'title': 'First Entry',
+        //             'month': 'March 23',
+        //         }
+            
+        //     ]
+        // },
+        // {
+        //     'year': '2021',
+        //     'events':[
+        //         {
+        //             'title': '32323',
+        //             'month': '01-01-2019',
+        //         },
+        //         {
+        //             'title': '2323 Entry',
+        //             'month': '01-01-2019',
+        //         }
+            
+        //     ]
+        // }
+        
+    ],
+
+
+
+    'awards': [
         {
             'year': '2023',
-            'event':[
+            'events':[
                 {
-                    'title': 'First Entry',
-                    'month': 'March 23',
+                    'title': 'Best project demonstration, Long Distance Face and Irirs Capture using AI-enabled PTZ camera, Awarded by Center for Identification Technology Research (CITeR), USA.',
+                    
                 },
-                {
-                    'title': 'First Entry',
-                    'month': 'March 23',
-                }
             
             ]
         },
         {
             'year': '2022',
-            'event':[
+            'events':[
                 {
-                    'title': 'First Entry',
-                    'month': 'March 23',
+                    'title': 'Technology showcase 2.5k, "VISION CONTROLLED PROSTHETIC HAND", Awarded by Clarkson University, USA.',
+                    
                 },
                 {
-                    'title': 'First Entry',
-                    'month': 'March 23',
-                }
+                    'title': 'President’s Challenge 2022, 1st, 2nd, 3rd for three segments for "VISION CONTROLLED PROSTHETIC HAND", Awarded by Clarkson, USA',
+                    
+                },
             
             ]
         },
         {
             'year': '2021',
-            'event':[
+            'events':[
                 {
-                    'title': 'First Entry',
-                    'date': '01-01-2019',
+                    'title': 'President’s Challenge 2021, Best Project demonstration in video on SMART THRONE, Awarded by Clarkson University, USA.',
                 },
+            
+            ]
+        },
+        {
+            'year': '2019',
+            'events':[
                 {
-                    'title': 'First Entry',
-                    'date': '01-01-2019',
-                }
+                    'title': 'Employee of the month, Awarded by PRAN-RFL group, Bangladesh.',
+                },
             
             ]
         }
-        
-    ],
+
+
+    ]
     
 
 }
