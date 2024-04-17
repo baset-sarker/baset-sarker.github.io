57517178b1c045dbc7adb63338e97ba6aa590668
baset-sarker <chayan2005@gmail.com>
2024-04-16 21:28:27 -0400

    updated news

--- js/data.js
+++ js/data.js
@@ -196,6 +196,10 @@ var data = {
             'year': '2024',
             'events':[
                 {
+                    'month': 'April 2024',
+                    'title': '<b>"Awarded Fellowship 2024"</b>, (05 April 2024) NYS Center of Excellence in Healthy Water Solutions Fellowship 2024',  
+                },
+                {
                     'month': 'March 2024',
                     'title': '<b>"Journal paper accepted"</b>, (22 March 2024) M.A.B. Sarker,S.M.S Hossain, N.G. Venkataswamy, S. Schuckers, and M. H. Imtiaz, An Open-Source Face-Aware Capture System, Electronics 2024, MDPI',  
                 },
